57f6a54c8c2ad04387d216a0f016ff99c7209ded
giamir <giamir.buoncristiani@gmail.com>
2016-01-04 15:48:00 +0000

    create class Airport and relative unit test

---
+++ jasmine/spec/airportSpec.js
@@ -0,0 +1,14 @@
+describe('Airport', function() {
+
+  var airport;
+
+  beforeEach(function() {
+    airport = new Airport();
+  });
+
+  // describe('', function() {
+  //   it('', function() {
+  //     expect(fizzBuzz.play(3)).toEqual('Fizz');
+  //   });
+  // });
+});
