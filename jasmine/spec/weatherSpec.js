cd6f9bb838e0d1d80c6eef3cc7c5493e233f8f32
giamir <giamir.buoncristiani@gmail.com>
2016-01-04 17:19:18 +0000

    define and implement isStormy method for Weather

--- jasmine/spec/weatherSpec.js
+++ jasmine/spec/weatherSpec.js
@@ -6,4 +6,14 @@ describe('Weather', function() {
     weather = new Weather();
   });
 
+  describe('#isStormy', function() {
+    it('can be non-stormy', function() {
+      spyOn(Math, 'random').and.returnValue(0.9);
+      expect(weather.isStormy()).toBe(false);
+    });
+    it('can be stormy', function() {
+      spyOn(Math, 'random').and.returnValue(0.1);
+      expect(weather.isStormy()).toBe(true);
+    });
+  });
 });
--- jasmine/src/weather.js
+++ jasmine/src/weather.js
@@ -1,2 +1,6 @@
 function Weather() {
 }
+
+Weather.prototype.isStormy = function() {
+  return (Math.floor(Math.random() * 3) === 0);
+};
