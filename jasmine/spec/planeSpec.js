dc6c02485806da5509b0dcd5ac9a89bd829006d8
giamir <giamir.buoncristiani@gmail.com>
2016-01-04 15:43:33 +0000

    create class Plane and relative unit test

---
+++ jasmine/spec/planeSpec.js
@@ -0,0 +1,14 @@
+describe('Plane', function() {
+
+  var plane;
+
+  beforeEach(function() {
+    plane = new Plane();
+  });
+
+  // describe('', function() {
+  //   it('', function() {
+  //     expect(fizzBuzz.play(3)).toEqual('Fizz');
+  //   });
+  // });
+});
