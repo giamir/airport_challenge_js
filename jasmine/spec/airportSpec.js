2202f40c5aae05f3b76a71de97ecd575dafd6d36
giamir <giamir.buoncristiani@gmail.com>
2016-01-04 16:50:06 +0000

    define and implement take_off method for Airport

--- jasmine/spec/airportSpec.js
+++ jasmine/spec/airportSpec.js
@@ -18,4 +18,17 @@ describe('Airport', function() {
       expect(airport.planes).toContain(plane);
     });
   });
+
+  describe('#take_off', function() {
+    beforeEach(function() {
+      airport.land(plane);
+      airport.take_off(plane);
+    });
+    it('instructs the plane to take off', function() {
+      expect(plane.take_off).toHaveBeenCalled();
+    });
+    it('has the plane after it has landed', function() {
+      expect(airport.planes).not.toContain(plane);
+    });
+  });
 });
--- jasmine/src/airport.js
+++ jasmine/src/airport.js
@@ -6,3 +6,11 @@ Airport.prototype.land = function(plane) {
   plane.land();
   this.planes.push(plane);
 };
+
+Airport.prototype.take_off = function(plane) {
+  plane.take_off();
+  var i = this.planes.indexOf(plane);
+  if(i != -1) {
+  	this.planes.splice(i, 1);
+  }
+};
